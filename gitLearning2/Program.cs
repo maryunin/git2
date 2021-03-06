0461c919a7313a0cc2aec1cef5f22b06c8c5034f
maryunin <maryunin@gmail.com>
2021-03-06 17:19:44 +0200

    first rdp commit

--- gitLearning2/Program.cs
+++ gitLearning2/Program.cs
@@ -11,6 +11,7 @@ namespace gitLearning2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi from rdp.");
             Console.ReadKey();
         }
     }
