4230e26b1c311908511a79ca35991673b6ecd84c
maryunin <maryunin@gmail.com>
2021-03-06 17:28:53 +0200

    4th feature

--- gitLearning2/Program.cs
+++ gitLearning2/Program.cs
@@ -13,6 +13,7 @@ namespace gitLearning2
             Console.WriteLine("Hello World!");
             Console.WriteLine("How is going there?");//1st feature
             Console.WriteLine("Its 2nd feature");//2nd feature
+            Console.WriteLine("Its 4th feature");//4th feature
             Console.ReadKey();
         }
     }
