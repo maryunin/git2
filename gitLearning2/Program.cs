9681159f194aa6f4d79d8b11eb55bd8a945a7d88
maryunin <maryunin@gmail.com>
2021-03-06 19:11:02 +0200

    8th rdp

--- gitLearning2/Program.cs
+++ gitLearning2/Program.cs
@@ -17,6 +17,7 @@ namespace gitLearning2
             Console.WriteLine("Its 5th feature");
             Console.WriteLine("Its 6th feature");
             Console.WriteLine("Its 7th feature");
+            Console.WriteLine("Its 8th feature");
             Console.ReadKey();
         }
     }
