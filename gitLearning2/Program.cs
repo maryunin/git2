69e02eed1190337298516cdb2401ea118da838d2
maryunin <maryunin@gmail.com>
2021-06-02 20:24:34 +0300

    f4 commit1

--- gitLearning2/Program.cs
+++ gitLearning2/Program.cs
@@ -18,6 +18,7 @@ namespace gitLearning2
             Console.WriteLine("develop changes 3");
             Console.WriteLine("feature2 1");
             Console.WriteLine("feature3 1");
+            Console.WriteLine("feature4 1");
             Console.ReadKey();
         }
     }
